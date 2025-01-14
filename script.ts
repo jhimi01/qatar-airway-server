28a49cc7045bb8287a70d8710bc2afd46894beee
jhimi01 <talukderjhimi@gmail.com>
2025-01-14 12:52:18 +0600

    sign up

--- script.ts
+++ script.ts
@@ -1,15 +1,18 @@
 import express from "express";
-var cors = require('cors')
+import cors from "cors";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
-const.use(cors())
-const app = express();
 const prisma = new PrismaClient();
 const port = 5000;
+const app = express();
 
-// node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"
+// const.use(cors())
+app.use(express.json());
+app.use(cors());
+
+//for generate token and set in the .env file--- node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"
 
 app.use(express.json()); // Parse JSON request bodies
 
@@ -49,7 +52,7 @@ app.post("/api/signup", async (req: any, res: any) => {
       firstName,
       lastName,
       title,
-      dateOfBirth,
+      dateOfBirth: new Date(dateOfBirth).toISOString(),
       gender,
       countryCode,
       mobileNumber,
@@ -87,11 +90,16 @@ app.post("/api/login", async (req: any, res: any) => {
 
   const token = jwt.sign(
     { userId: user.id, email: user.email },
-    "your_secret_key", // Secret key (should be an environment variable)
-    { expiresIn: "1h" }
+    "process.env.JWT_SECRET_KEY", // Secret key (should be an environment variable)
+    { expiresIn: "7d" }
   );
 
   res.json({ message: "Logged in successfully", token });
+});
+
+app.get("/users", async (req: any, res: any) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
 });
 
 app.listen(port, () => {
