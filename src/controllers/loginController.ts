2bd65921d6e097aa15626a0b4c39f1634e180f4f
jhimi01 <talukderjhimi@gmail.com>
2025-01-16 13:08:32 +0600

    refetch

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -5,56 +5,6 @@ import { generateOTP } from "../utils/otp";
 import { sendOTPEmail } from "../utils/email";
 
 // login action
-// export const login = async (req: any, res: any) => {
-//   const { email, password, recaptchaToken } = req.body;
-
-//   // 1. Verify reCAPTCHA token with Google's API
-//   try {
-//     const response = await axios.post(
-//       `https://www.google.com/recaptcha/api/siteverify`,
-//       null,
-//       {
-//         params: {
-//           secret: process.env.RECAPTCHA_SECRET_KEY,
-//           response: recaptchaToken,
-//         },
-//       }
-//     );
-
-//     console.log("response of recatpha", response);
-
-//     if (!response.data.success) {
-//       return res.status(400).json({ error: "reCAPTCHA verification failed" });
-//     }
-
-//     // 2. Find the user in the database
-//     const user = await prisma.user.findUnique({ where: { email } });
-//     if (!user) {
-//       return res.status(401).json({ error: "Invalid email or password" });
-//     }
-
-//     // 3. Compare password (if using hashed passwords)
-//     const match = await bcrypt.compare(password, user.password);
-//     if (!match) {
-//       return res.status(401).json({ error: "Invalid email or password" });
-//     }
-
-//     // 4. Generate a JWT token
-//     const token = jwt.sign(
-//       { userId: user.id, email: user.email },
-//       "process.env.JWT_SECRET_KEY",
-//       { expiresIn: "7d" }
-//     );
-
-//     // 5. Send the token back in the response
-//     return res.status(200).json({ token });
-//   } catch (error) {
-//     console.error("Error verifying reCAPTCHA:", error);
-//     return res.status(500).send("Error verifying reCAPTCHA");
-//   }
-// };
-
-// login action
 export const login = async (req: any, res: any) => {
   const { email, password, recaptchaToken } = req.body;
 
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,6 +5,7 @@ import { verifyOTPLogin } from "../controllers/verifyOtpLogin";
 import { getUserData } from "../controllers/loggedinUserController";
 import { editProfileController } from "../controllers/editProfileController";
 import { imageEditController } from "../controllers/imageEditController";
+import { logOutController } from "../controllers/logOutController";
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.post("/verify-otp-login", verifyOTPLogin);
 router.get("/loggedin-user", getUserData);
 router.patch("/edit-profile", editProfileController);
 router.patch("/edit-image", imageEditController);
+router.delete("/logout", logOutController);
 
 
 export default router;
